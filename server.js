923c4f576c3b95c18e4b5ce79926b0c9172efd22
pwangy <blipsandclicks@gmail.com>
2024-03-09 15:21:15 -0800

    retry render deploy

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('./db.json')
-const middlewares = jsonServer.defaults({ static: './build' })
-const PORT = process.env.PORT || 4000
+// const middlewares = jsonServer.defaults({ static: './build' })
+const port = process.env.PORT || 4000
 
 server.use(middlewares)
-server.use(jsonServer.rewriter({ '/api/*': '/$1' }))
+// server.use(jsonServer.rewriter({ '/api/*': '/$1' }))
 server.use(router)
-server.listen(PORT)
+
+server.listen(port)
